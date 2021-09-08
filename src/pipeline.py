ab957317956d20fd1fed317203d2721665a4ed5f
Tomoyuki Nohara <peace098beat.t@gmail.com>
2021-09-08 23:51:24 +0900

    Add Dir action (#1)
    * Add Dir action
    
    * edit typo

--- src/pipeline.py
+++ src/pipeline.py
@@ -5,19 +5,29 @@
     > python main.py --target ../data/1_test.wav --outdir ../outputs
 
 """
-import argparse
+from pathlib import Path
 
 import monolizer
 import featurizer
 
+from logging import getLogger
+logger = getLogger(__name__)
+
+logger.debug(__name__)
 
 def run(in_file):
     
     # 1. Monolize
-    mono_file = monolizer.to_mono(in_file)
+    try:
+        mono_file = monolizer.to_mono(in_file)
+    except KeyboardInterrupt:
+        return []
 
     # 2. Featurize
-    fig_path, json_path, sig_path = featurizer.run(mono_file)
+    try:
+        fig_path, json_path, sig_path = featurizer.run(mono_file)
+    except KeyboardInterrupt:
+        return [mono_file]
 
     outputs = [
         mono_file,
@@ -27,6 +37,51 @@ def run(in_file):
     ]
 
     return outputs
+
+import glob
+import shutil
+def run_dir(in_dir, ext, out_dir):
+
+    assert Path(in_dir).exists(), in_dir
+    assert ext.startswith("."), ext
+
+    audio_p_s = glob.glob(f"{str(in_dir)}/*{ext}")
+
+    N = len(list(audio_p_s))
+    assert N > 0
+
+    out_files_s = []
+    dist_files_s = []
+
+    try:
+        for n, audio_p in enumerate(audio_p_s):
+
+            logger.debug(f"[{n}/{N}]: {audio_p}")
+
+            out_files = run(audio_p)
+
+            out_files_s += out_files
+
+        for out_file in out_files_s:
+            assert Path(out_file).exists(), out_file
+            dist_p = Path(out_dir) / Path(out_file).name
+            a = shutil.move(out_file, dist_p)
+            assert Path(a).exists(), a
+            dist_files_s.append(a)
+
+    except KeyboardInterrupt:
+        rm_files = out_files_s + dist_files_s
+        print(rm_files)
+        for rm_file in rm_files:
+            if Path(rm_file).exists():
+                Path(rm_file).unlink()
+
+        import sys
+        sys.exit(0)
+
+
+
+    return out_files_s
 
 
 # def get_arg():
---
+++ tests/test_pipeline_dir.py
@@ -0,0 +1,56 @@
+# test_pipeline_dir.py
+
+from pathlib import Path
+import tempfile
+import shutil
+
+import pytest
+import pipeline
+
+
+work_dir = Path(__file__).parent.resolve()
+
+
+def test_pipeline_dir():
+
+    in_dir = "data"
+    ext = "wav"
+    out_dir = "outputs/test_pipeline_dir"
+
+
+    # Setup in_dir
+    in_dir = Path(work_dir, in_dir)
+    assert Path(in_dir).exists()
+
+    # Setup ext
+    if not ext.startswith("."):
+        ext = f".{ext}"
+
+    # Setup OutDir
+    out_dir = Path(work_dir) / out_dir
+
+    if Path(out_dir).exists():
+        shutil.rmtree(out_dir)
+    assert Path(out_dir).exists() == False
+
+    Path(out_dir).mkdir(exist_ok=True, parents=True)
+    assert Path(out_dir).exists(), out_dir
+
+    n_audios = len(list(Path(in_dir).glob(f"*{ext}")))
+    assert n_audios > 0
+
+    # TEST
+    pipeline.run_dir(in_dir, ext, out_dir)
+
+    n_outfiles = len(list(Path(out_dir).glob(f"*")))
+    assert n_outfiles > 0
+
+    n_mono = len(list(Path(out_dir).glob("*.mono.*")))
+    assert n_audios == n_mono / 4
+
+    n_json = len(list(Path(out_dir).glob("*.json")))
+    assert n_audios == n_json
+
+
+if __name__ == '__main__':
+    print(__FILE__)
